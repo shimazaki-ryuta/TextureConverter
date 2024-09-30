99e477ae24dab738c85b4e67f76d029d66483b1a
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2024-09-30 15:04:54 +0900

    エンコード形式変更

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <cstdlib>
 int main(int argc,char* argv[]) {
 
-	printf("Hello World!\n");
-	
+	for (int i = 0; i < argc;i++) {
+		printf(argv[i]);
+		printf("\n");
+	}
 	system("pause");
 	return 0;
 }
